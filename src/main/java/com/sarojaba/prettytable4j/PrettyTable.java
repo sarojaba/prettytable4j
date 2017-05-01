45ad1750f09087ec085a45e3a34a55147f22b416
sarojaba <sarojaba@gmail.com>
2017-05-01 20:19:08 +0900

    First Code Commit

---
+++ src/main/java/com/sarojaba/prettytable4j/PrettyTable.java
@@ -0,0 +1,167 @@
+package com.sarojaba.prettytable4j;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.text.NumberFormat;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class PrettyTable {
+
+    private List<String> fieldNames = new ArrayList<>();
+
+    private List<Object[]> rows = new ArrayList<>();
+
+    private boolean comma = false;
+
+    public PrettyTable fieldNames(String... fieldNames) {
+        this.fieldNames.addAll(Arrays.asList(fieldNames));
+        return this;
+    }
+
+    public PrettyTable addRow(Object... row) {
+        this.rows.add(row);
+        return this;
+    }
+
+    public PrettyTable deleteRow(int num) {
+        this.rows.remove(num - 1);
+        return this;
+    }
+
+    public PrettyTable comma(boolean comma) {
+        this.comma = comma;
+        return this;
+    }
+
+    public PrettyTable sortTable(String fieldName) {
+        return sortTable(fieldName, false);
+    }
+
+    public PrettyTable sortTable(String fieldName, boolean reverse) {
+        int idx = Collections.binarySearch(fieldNames, fieldName);
+
+        rows.sort(new Comparator<Object[]>() {
+            @Override
+            public int compare(Object[] o1, Object[] o2) {
+
+                if (o1[idx] instanceof Comparable && o2[idx] instanceof Comparable) {
+                    int c = ((Comparable) o1[idx]).compareTo((Comparable) o2[idx]);
+                    return c * (reverse ? -1 : 1);
+                }
+                return 0;
+            }
+        });
+
+        return this;
+    }
+
+    public PrettyTable clearTable() {
+        rows.clear();
+        return this;
+    }
+
+    public PrettyTable deleteTable() {
+        rows.clear();
+        fieldNames.clear();
+        return this;
+    }
+
+    @Override
+    public String toString() {
+
+        // Check empty
+        if (fieldNames.isEmpty()) {
+            return "";
+        }
+
+        int[] maxWidth = adjustMaxWidth();
+
+        StringBuilder sb = new StringBuilder();
+
+        String line = line(maxWidth);
+
+        // Draw top border line
+        sb.append(line);
+
+        // Convert headers to table
+        sb.append("\n");
+        sb.append("|");
+
+        for (int i = 0; i < fieldNames.size(); i++) {
+            String nh = StringUtils.rightPad(fieldNames.get(i), maxWidth[i]);
+            sb.append(String.format(" %s ", nh));
+            sb.append("|");
+        }
+
+        // Draw line
+        sb.append("\n");
+        sb.append(line);
+
+        // Convert rows to table
+        rows.forEach(r -> {
+            sb.append("\n");
+            sb.append("|");
+            for (int c = 0; c < r.length; c++) {
+
+                String nc;
+                if (r[c] instanceof Number) {
+                    String n = comma ?
+                            NumberFormat.getNumberInstance(Locale.US).format(r[c]):
+                            r[c].toString();
+                    nc = StringUtils.leftPad(n, maxWidth[c]);
+                } else {
+                    nc = StringUtils.rightPad(r[c].toString(), maxWidth[c]);
+                }
+
+                sb.append(String.format(" %s ", nc));
+                sb.append("|");
+            }
+        });
+
+        // Draw bottom border line
+        sb.append("\n");
+        sb.append(line);
+
+        return sb.toString();
+    }
+
+    /*
+     * Adjust for max width of the column
+     */
+    private int[] adjustMaxWidth() {
+
+        // Adjust comma
+        List<List<String>> converted = rows.stream()
+                .map(r -> Stream.of(r).map(o -> {
+                    if (comma && o instanceof Number) {
+                        return NumberFormat.getNumberInstance(Locale.US).format(o);
+                    } else {
+                        return o.toString();
+                    }
+                }).collect(Collectors.toList())).collect(Collectors.toList());
+
+        return IntStream.range(0, fieldNames.size())
+                .map(i -> {
+                    int n = converted.stream()
+                            .map(f -> f.get(i).length())
+                            .max(Comparator.naturalOrder())
+                            .orElse(0);
+                    return Math.max(fieldNames.get(i).length(), n);
+                }).toArray();
+    }
+
+    private String line(int[] maxWidth) {
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("+");
+        for (int i = 0; i < fieldNames.size(); i++) {
+            sb.append(StringUtils.rightPad("", maxWidth[i] + 2, '-'));
+            sb.append("+");
+        }
+        return sb.toString();
+    }
+}
---
+++ src/test/java/com/sarojaba/prettytable4j/PrettyTableTest.java
@@ -0,0 +1,251 @@
+package com.sarojaba.prettytable4j;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PrettyTableTest {
+
+    @Test
+    public void testEmpty() {
+
+        PrettyTable pt = new PrettyTable();
+
+        assertEquals("", pt.toString());
+    }
+
+    @Test
+    public void testCreate() {
+
+        PrettyTable pt = new PrettyTable()
+                .fieldNames("name", "age", "city")
+                .addRow("john", 22, "new york")
+                .addRow("elizabeth", 43, "chicago")
+                .addRow("bill", 31, "atlanta")
+                .addRow("mary", 18, "los angeles");
+
+        assertEquals("+-----------+-----+-------------+\n" +
+                "| name      | age | city        |\n" +
+                "+-----------+-----+-------------+\n" +
+                "| john      |  22 | new york    |\n" +
+                "| elizabeth |  43 | chicago     |\n" +
+                "| bill      |  31 | atlanta     |\n" +
+                "| mary      |  18 | los angeles |\n" +
+                "+-----------+-----+-------------+", pt.toString());
+    }
+
+    @Test
+    public void testFieldNames() {
+
+        PrettyTable pt = new PrettyTable()
+                .fieldNames("City name", "Area", "Population", "ann");
+
+        assertEquals("+-----------+------+------------+-----+\n" +
+                "| City name | Area | Population | ann |\n" +
+                "+-----------+------+------------+-----+\n" +
+                "+-----------+------+------------+-----+", pt.toString());
+    }
+
+    @Test
+    public void testAddRow() {
+
+        PrettyTable pt = new PrettyTable()
+                .fieldNames("City name", "Area", "Population", "ann")
+                .addRow("Adelaide",1295, 1158259, 600.5)
+                .addRow("Brisbane",5905, 1857594, 1146.4)
+                .addRow("Darwin", 112, 120900, 1714.7)
+                .addRow("Hobart", 1357, 205556, 619.5)
+                .addRow("Sydney", 2058, 4336374, 1214.8)
+                .addRow("Melbourne", 1566, 3806092, 646.9)
+                .addRow("Perth", 5386, 1554769, 869.4);
+
+        assertEquals("+-----------+------+------------+--------+\n" +
+                "| City name | Area | Population | ann    |\n" +
+                "+-----------+------+------------+--------+\n" +
+                "| Adelaide  | 1295 |    1158259 |  600.5 |\n" +
+                "| Brisbane  | 5905 |    1857594 | 1146.4 |\n" +
+                "| Darwin    |  112 |     120900 | 1714.7 |\n" +
+                "| Hobart    | 1357 |     205556 |  619.5 |\n" +
+                "| Sydney    | 2058 |    4336374 | 1214.8 |\n" +
+                "| Melbourne | 1566 |    3806092 |  646.9 |\n" +
+                "| Perth     | 5386 |    1554769 |  869.4 |\n" +
+                "+-----------+------+------------+--------+", pt.toString());
+    }
+
+    @Test
+    public void testComma() {
+
+        PrettyTable pt = new PrettyTable()
+                .fieldNames("City name", "Area", "Population", "ann")
+                .addRow("Adelaide",1295, 1158259, 600.5)
+                .addRow("Brisbane",5905, 1857594, 1146.4)
+                .addRow("Darwin", 112, 120900, 1714.7)
+                .addRow("Hobart", 1357, 205556, 619.5)
+                .addRow("Sydney", 2058, 4336374, 1214.8)
+                .addRow("Melbourne", 1566, 3806092, 646.9)
+                .addRow("Perth", 5386, 1554769, 869.4)
+                .comma(true);
+
+        assertEquals("+-----------+-------+------------+---------+\n" +
+                "| City name | Area  | Population | ann     |\n" +
+                "+-----------+-------+------------+---------+\n" +
+                "| Adelaide  | 1,295 |  1,158,259 |   600.5 |\n" +
+                "| Brisbane  | 5,905 |  1,857,594 | 1,146.4 |\n" +
+                "| Darwin    |   112 |    120,900 | 1,714.7 |\n" +
+                "| Hobart    | 1,357 |    205,556 |   619.5 |\n" +
+                "| Sydney    | 2,058 |  4,336,374 | 1,214.8 |\n" +
+                "| Melbourne | 1,566 |  3,806,092 |   646.9 |\n" +
+                "| Perth     | 5,386 |  1,554,769 |   869.4 |\n" +
+                "+-----------+-------+------------+---------+", pt.toString());
+    }
+
+    @Test
+    public void testSortTableByInteger() {
+
+        PrettyTable pt = new PrettyTable()
+                .fieldNames("name", "age", "city")
+                .addRow("john", 22, "new york")
+                .addRow("elizabeth", 43, "chicago")
+                .addRow("bill", 31, "atlanta")
+                .addRow("mary", 18, "los angeles")
+                .sortTable("age");
+
+        assertEquals("+-----------+-----+-------------+\n" +
+                "| name      | age | city        |\n" +
+                "+-----------+-----+-------------+\n" +
+                "| mary      |  18 | los angeles |\n" +
+                "| john      |  22 | new york    |\n" +
+                "| bill      |  31 | atlanta     |\n" +
+                "| elizabeth |  43 | chicago     |\n" +
+                "+-----------+-----+-------------+", pt.toString());
+    }
+
+    @Test
+    public void testSortTableByIntegerReverse() {
+
+        PrettyTable pt = new PrettyTable()
+                .fieldNames("name", "age", "city")
+                .addRow("john", 22, "new york")
+                .addRow("elizabeth", 43, "chicago")
+                .addRow("bill", 31, "atlanta")
+                .addRow("mary", 18, "los angeles")
+                .sortTable("age", true);
+
+        assertEquals("+-----------+-----+-------------+\n" +
+                "| name      | age | city        |\n" +
+                "+-----------+-----+-------------+\n" +
+                "| elizabeth |  43 | chicago     |\n" +
+                "| bill      |  31 | atlanta     |\n" +
+                "| john      |  22 | new york    |\n" +
+                "| mary      |  18 | los angeles |\n" +
+                "+-----------+-----+-------------+", pt.toString());
+    }
+
+    @Test
+    public void testSortTableByFloat() {
+
+        PrettyTable pt = new PrettyTable()
+                .fieldNames("City name", "Area", "Population", "ann")
+                .addRow("Adelaide",1295, 1158259, 600.5)
+                .addRow("Brisbane",5905, 1857594, 1146.4)
+                .addRow("Darwin", 112, 120900, 1714.7)
+                .addRow("Hobart", 1357, 205556, 619.5)
+                .addRow("Sydney", 2058, 4336374, 1214.8)
+                .addRow("Melbourne", 1566, 3806092, 646.9)
+                .addRow("Perth", 5386, 1554769, 869.4)
+                .sortTable("ann");
+
+        assertEquals("+-----------+------+------------+--------+\n" +
+                "| City name | Area | Population | ann    |\n" +
+                "+-----------+------+------------+--------+\n" +
+                "| Adelaide  | 1295 |    1158259 |  600.5 |\n" +
+                "| Hobart    | 1357 |     205556 |  619.5 |\n" +
+                "| Melbourne | 1566 |    3806092 |  646.9 |\n" +
+                "| Perth     | 5386 |    1554769 |  869.4 |\n" +
+                "| Brisbane  | 5905 |    1857594 | 1146.4 |\n" +
+                "| Sydney    | 2058 |    4336374 | 1214.8 |\n" +
+                "| Darwin    |  112 |     120900 | 1714.7 |\n" +
+                "+-----------+------+------------+--------+", pt.toString());
+    }
+
+    @Test
+    public void testDeleteRow() {
+
+        PrettyTable pt = new PrettyTable()
+                .fieldNames("name", "age", "city")
+                .addRow("john", 22, "new york")
+                .addRow("elizabeth", 43, "chicago")
+                .addRow("bill", 31, "atlanta")
+                .addRow("mary", 18, "los angeles");
+
+        assertEquals("+-----------+-----+-------------+\n" +
+                "| name      | age | city        |\n" +
+                "+-----------+-----+-------------+\n" +
+                "| john      |  22 | new york    |\n" +
+                "| elizabeth |  43 | chicago     |\n" +
+                "| bill      |  31 | atlanta     |\n" +
+                "| mary      |  18 | los angeles |\n" +
+                "+-----------+-----+-------------+", pt.toString());
+
+        pt.deleteRow(2);
+        pt.deleteRow(1);
+
+        assertEquals("+------+-----+-------------+\n" +
+                "| name | age | city        |\n" +
+                "+------+-----+-------------+\n" +
+                "| bill |  31 | atlanta     |\n" +
+                "| mary |  18 | los angeles |\n" +
+                "+------+-----+-------------+", pt.toString());
+    }
+
+    @Test
+    public void testClearTable() {
+
+        PrettyTable pt = new PrettyTable()
+                .fieldNames("name", "age", "city")
+                .addRow("john", 22, "new york")
+                .addRow("elizabeth", 43, "chicago")
+                .addRow("bill", 31, "atlanta")
+                .addRow("mary", 18, "los angeles");
+
+        assertEquals("+-----------+-----+-------------+\n" +
+                "| name      | age | city        |\n" +
+                "+-----------+-----+-------------+\n" +
+                "| john      |  22 | new york    |\n" +
+                "| elizabeth |  43 | chicago     |\n" +
+                "| bill      |  31 | atlanta     |\n" +
+                "| mary      |  18 | los angeles |\n" +
+                "+-----------+-----+-------------+", pt.toString());
+
+        pt.clearTable();
+
+        assertEquals("+------+-----+------+\n" +
+                "| name | age | city |\n" +
+                "+------+-----+------+\n" +
+                "+------+-----+------+", pt.toString());
+    }
+
+    @Test
+    public void testDeleteTable() {
+
+        PrettyTable pt = new PrettyTable()
+                .fieldNames("name", "age", "city")
+                .addRow("john", 22, "new york")
+                .addRow("elizabeth", 43, "chicago")
+                .addRow("bill", 31, "atlanta")
+                .addRow("mary", 18, "los angeles");
+
+        assertEquals("+-----------+-----+-------------+\n" +
+                "| name      | age | city        |\n" +
+                "+-----------+-----+-------------+\n" +
+                "| john      |  22 | new york    |\n" +
+                "| elizabeth |  43 | chicago     |\n" +
+                "| bill      |  31 | atlanta     |\n" +
+                "| mary      |  18 | los angeles |\n" +
+                "+-----------+-----+-------------+", pt.toString());
+
+        pt.deleteTable();
+
+        assertEquals("", pt.toString());
+    }
+}
